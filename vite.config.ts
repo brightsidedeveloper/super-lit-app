124d4e75d0a8b6d186c54b4cd6583d9fd98f7b17
Tim <tim@brightsidedeveloper.com>
2024-04-30 17:26:16 -0500

    packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [react(), TanStackRouterVite()],
 })
