bcb4165f8716a7e4a9b3eafa5a30a9cb61e19e27
Tim <tim@brightsidedeveloper.com>
2024-05-11 08:48:13 -0500

    Go

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,4 @@
-import { createRootRouteWithContext, Link, Outlet } from '@tanstack/react-router'
+import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import { initializeTheme, mediaThemeEventListener, storageThemeEventListener } from '../utils/darkMode'
 
@@ -14,12 +14,12 @@ export const Route = createRootRouteWithContext()({
 function Root() {
   return (
     <>
-      <div className='p-2 flex gap-2'>
+      {/* <div className='p-2 flex gap-2'>
         <Link to='/' className='[&.active]:font-bold'>
           Home
         </Link>
       </div>
-      <hr />
+      <hr /> */}
       <Outlet />
       <TanStackRouterDevtools />
     </>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,14 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { setTheme } from '../utils/darkMode'
-
+import { motion } from 'framer-motion'
 export const Route = createFileRoute('/')({
   component: Home,
 })
 
 function Home() {
   return (
-    <div className='flex flex-col gap-4'>
-      <h1>Home</h1>
+    <motion.div className='flex flex-col gap-4'>
+      <h1 className='text-6xl'>Home</h1>
       <button className='dark:translate-x-0 translate-x-4 transition-transform' onClick={() => setTheme('light')}>
         Light
       </button>
@@ -18,6 +18,6 @@ function Home() {
       <button className='' onClick={() => setTheme(null)}>
         System
       </button>
-    </div>
+    </motion.div>
   )
 }
